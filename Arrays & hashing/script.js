aefba1285c7e5db24f741bdb3fa0d3d8fe3860fc
alex100588 <alexandru_ionasc@yahoo.com>
2023-06-12 16:20:26 +0300

    Contains Duplicate

--- Arrays & hashing/script.js
+++ Arrays & hashing/script.js
@@ -1,1 +1,17 @@
-console.log('yes');
+// 1. Contains Duplicate
+//  Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+let containsDuplicate = function (nums) {
+  nums.sort();
+  for (let i = 0; i < nums.length; i++) {
+    // console.log(nums[i], nums[i + 1]);
+    if (nums[i] === nums[i + 1]) {
+      return true;
+    } else {
+      nums[i]++;
+    }
+  }
+  return false;
+};
+
+console.log(containsDuplicate([2, 4, 5, 4]));
